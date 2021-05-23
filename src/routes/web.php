3c75d386cd9b2dbe658c4bbf5fdab21b2a173b27
make-money-master <money-001@mail.com>
2021-05-23 15:51:38 +0800

    更新路由

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OpenApiController;
 use App\Http\Controllers\ShareController;
 
 Route::get("/", ShareController::class . "@share");
 Route::get("/at", ShareController::class . "@share");
 Route::get("/xx-zw", HomeController::class . "@index");
+
+Route::get("/oapi", OpenApiController::class . "@index");
 
 
 
