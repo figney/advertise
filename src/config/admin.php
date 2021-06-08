33d657a42e6bdaa4f6ba502065cd91b1fb1c1d9a
make-money-master <money-001@mail.com>
2021-06-08 19:38:17 +0800

    修改上传驱动，添加到配置文件

--- src/config/admin.php
+++ src/config/admin.php
@@ -225,7 +225,7 @@ return [
     'upload' => [
 
         // Disk in `config/filesystem.php`.
-        'disk' => 'local',
+        'disk' => env('UPLOAD_DIRVER', 'local'),
 
         // Image and file upload path under the disk above.
         'directory' => [
